42f5cfec2cf833d5733d9356f6c70f335a29202e
Mateus Evaristo Melo <mateusevaristom@gmail.com>
2026-04-18 15:29:05 -0300

    Correção de erro nos dados iniciais e implementação de quantidade de despesas

--- public/script.js
+++ public/script.js
@@ -6,5 +6,21 @@ let renda = Number(prompt("Digite sua renda mensal: "));
 
 // Validação da renda
 while (isNaN(renda)) {
-    renda = Number(prompt("Quantas despesas deseja informar? (1 a 5)"));
+    renda = Number(prompt("Valor inválido. Digite sua senha novamente:"));
+}
+
+// ===== QUANTIDADE DE DESPESAS =====
+
+let quantidade = Number(prompt("Quantas despesas deseja informar? (1 a 5)"));
+
+// Validação
+while (isNaN(quantidade)) {
+    quantidade = Number(prompt("Digite um número válido entre 1 e 5:"));
+}
+
+//Limite entre 1 e 5
+if (quantidade < 1) {
+    quantidade = 1;
+} else if (quantidade > 5) {
+    quantidade = 5;
 }
