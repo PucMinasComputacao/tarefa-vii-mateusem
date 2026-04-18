16a17055e502f15535edab0e6930007e7aad42dc
Mateus Evaristo Melo <mateusevaristom@gmail.com>
2026-04-18 15:22:29 -0300

    Implementação dos dados iniciais (nome e renda com validação)

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
-//1. Dados inic
+// ===== DADOS INICIAIS =====
+
+let nome = prompt("Digite seu nome:");
+
+let renda = Number(prompt("Digite sua renda mensal: "));
+
+// Validação da renda
+while (isNaN(renda)) {
+    renda = Number(prompt("Quantas despesas deseja informar? (1 a 5)"));
+}
